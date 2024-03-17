6cdfefb5a5e719c7679b71e42aacd33fcb7e4e36
valeriya <valeriyastoyan26@gmail.com>
2024-03-17 22:29:05 +0300

    add Link and ch key

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -38,17 +39,17 @@ const ComicsList = () => {
     }
 
     function renderItems(arr) {
-        const items = arr.map((item, i) => {
+        const items = arr.map(item => {
 
             return (
                 <li 
                 className="comics__item"
-                key={i}>
-                    <a href="#">
+                key={item.id}>
+                    <Link to={`/comics/${item.id}`}>
                         <img src={item.thumbnail} className="comics__item-img" alt={item.title}/>
                         <div className="comics__item-name">{item.title}</div>
                         <div className="comics__item-price">{item.price}</div>
-                    </a>
+                    </Link>
                 </li>
             )
         });
