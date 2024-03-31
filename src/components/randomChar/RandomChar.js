af91ddc26d0365fbd5aa1b7537abac0d667ae623
valeriya <valeriyastoyan26@gmail.com>
2024-03-31 11:10:05 +0300

    corr mist

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -20,15 +20,15 @@ const RandomChar = () => {
         return () => {
             clearInterval(timerId);
         } 
-    }, []);
+    }, [])
 
     const onCharLoaded = (char) => {
         setChar(char);
-    };
+    }
 
     const updateChar = () => {
         clearError();
-        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
         
         getCharacter(id)
             .then(onCharLoaded);
@@ -37,7 +37,7 @@ const RandomChar = () => {
 
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? <View char={char}/> : null;
+    const content = !(loading || error|| !char) ? <View char={char}/> : null;
 
 
     return (
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -33,11 +33,13 @@ const useMarvelService = () => {
         return {
             id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description
+                    ? `${char.description.slice(0, 210)}...`
+                    : "There is no description for this character",
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
-            comics: char.comics.items
+            comics: char.comics.items,
         }
     }
 
