6e980ecaf490675401d9e30c7b9724ea92ff1382
valeriya <valeriyastoyan26@gmail.com>
2024-04-13 21:00:19 +0300

    add setProcess

--- src/components/charSearchForm/charSearchForm.js
+++ src/components/charSearchForm/charSearchForm.js
@@ -8,9 +8,10 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './CharSearchForm.scss';
 
+
 const CharSearchForm = () => {
     const [char, setChar] = useState(null);
-    const {loading, error, getCharacterByName, clearError} = useMarvelService();
+    const {getCharacterByName, clearError, process, setProcess} = useMarvelService();
 
     const onCharLoaded = (char) => {
         setChar(char);
@@ -20,10 +21,11 @@ const CharSearchForm = () => {
         clearError();
 
         getCharacterByName(name)
-            .then(onCharLoaded);
+            .then(onCharLoaded)
+            .then(() => setProcess('confirmed'))  
     }
 
-    const errorMessage = error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
+    const errorMessage = process === 'error' ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
     const results = !char ? null : char.length > 0 ?
                     <div className="char__search-wrapper">
                         <div className="char__search-success">There is! Visit {char[0].name} page?</div>
@@ -59,15 +61,15 @@ const CharSearchForm = () => {
                         <button 
                             type='submit' 
                             className="button button__main"
-                            disabled={loading}>
+                            disabled={process === 'loading'}>
                             <div className="inner">find</div>
                         </button>
                     </div>   
                 <FormikErrorMessage component="div" className="char__search-error" name="charName" /> 
             </Form>
         </Formik>
-        {results}
-        {errorMessage}
+            {results}
+            {errorMessage}   
         </div>
     )          
 }
